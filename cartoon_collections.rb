74e2ededb64a0dff08fb8009bcb4edde001e3887
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-26 17:08:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 
 def find_the_cheese(food)
   cheese = ["cheddar", "gouda", "camembert"]
-  result = food.each do |word| word.include?("cheese")
+  result = food.each do |word| word.any?("cheese")
 end
 result 
 end
